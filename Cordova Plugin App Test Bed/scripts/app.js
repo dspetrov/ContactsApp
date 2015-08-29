7e55c9356ba7210c5f64f2a15964d76dde35ee30
Dobromir Petrov <ds.petrov@abv.bg>
2015-08-29 12:17:32 +0000

    Changes made by Dobromir Petrov (ds.petrov@abv.bg).

--- Cordova Plugin App Test Bed/scripts/app.js
+++ Cordova Plugin App Test Bed/scripts/app.js
@@ -1,21 +1,77 @@
 (function () {
-    
+
     var app, selectedContact, selectedContactId;
 
-    document.addEventListener('deviceready', function () {  
+    document.addEventListener('deviceready', function () {
         navigator.splashscreen.hide();
-        app = new kendo.mobile.Application(document.body, {skin: 'flat', transition: 'slide'});
+        app = new kendo.mobile.Application(document.body, {
+            skin: 'flat',
+            transition: 'slide'
+        });
     }, false);
 
     function onError() {
+        debugger;
         alert("Sorry, but there was error!");
     }
-    
+
     // Handles iOS not returning displayName or returning null
     function getName(c) {
-    	if(c.name.formatted) return c.name.formatted;
-    	if(c.name.givenName && c.name.familyName) return c.name.givenName + " " + c.name.familyName;
-    	return "No Name Listed";
+        if (c.name.formatted) return c.name.formatted;
+        if (c.name.givenName && c.name.familyName) return c.name.givenName + " " + c.name.familyName;
+        return "No Name Listed";
+    }
+
+    window.getAllContacts = function () {
+        var options = new ContactFindOptions();
+        options.filter = "";
+        options.multiple = true;
+        var fields = ["*"];
+        navigator.contacts.find(fields, onContactSuccess, onError, options);
+    }
+
+    function onContactSuccess(contacts) {
+        var template = kendo.template($("#contacts-template").html());
+        var dataSource = new kendo.data.DataSource({
+            data: contacts
+        });
+        dataSource.bind("change", function () {
+            $("#contacts-list").html(kendo.render(template, dataSource.view()));
+        });
+        dataSource.read();
+    }
+
+    window.getContactDetails = function (e) {
+        selectedContactId = e.view.params.id;
+        var options = new ContactFindOptions();
+        options.filter = e.view.params.id;
+        options.multiple = true;
+        var fields = ["*"];
+        navigator.contacts.find(fields, onContactDetailSuccess, onError, options);
+    }
+
+    function onContactDetailSuccess(contacts) {
+        for (var i = 0; i < contacts.length; i++) {
+            if (contacts[i].id == selectedContactId) {
+                $("#contact-name").text(getName(contacts[i]));
+
+                if (contacts[i].phoneNumbers) {
+                    $("#contact-phone").text(contacts[i].phoneNumbers[0].value);
+                } else {
+                    $("#contact-phone").text("");
+                }
+
+                if (contacts[i].photos && contacts[i].photos.length) {
+                    $(".largeProfile").attr("src", contacts[i].photos[0].value);
+                } else {
+                    $(".largeProfile").attr("src", "styles/blankProfile.png");
+                }
+
+                selectedContact = contacts[i];
+
+                break;
+            }
+        }
     }
 
 }());
